53ed2b11b6fa537b7ac667023817b2dd3828ad62
leechangsin <ycs318@naver.com>
2016-08-24 18:30:26 +0900

    #3 회원관련 기능 구현 - DO, DAO   1. pom.xml     - 로깅을 위한 log4j, slf4j-api, slf4j-log4j 의존성 추가   2. log4j.xml     - 7번째줄 : 오타 수정     - 19번째줄 : 오타 수정     - 나머지 자동 줄 정렬 기능을 이용한 약간의 코드정렬   3. Member     - DO   4. MemberDao     - DAO

---
+++ sns/src/main/java/sns/member/Member.java
@@ -0,0 +1,52 @@
+package sns.member;
+
+import java.sql.Date;
+
+public class Member {
+	private String name;
+	private String id;
+	private String passwd;
+	private String email;
+	private Date date;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPasswd() {
+		return passwd;
+	}
+
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+}
