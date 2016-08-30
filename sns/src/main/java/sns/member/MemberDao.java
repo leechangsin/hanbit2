4b6656dafc996346d76885a8f577e7ec7d512884
leechangsin <ycs318@naver.com>
2016-08-30 17:18:03 +0900

    #5 컨트롤러 구현   1. MemberDao     - DBManager클래스가 추가되 이를 MemberDao가 사용하도록 변경
      2. user_control
        - 컨트롤러 구현 중 미완성
        - MemberDao에 누락된 기능을 발견하여 잠시 중지

--- sns/src/main/java/sns/member/MemberDao.java
+++ sns/src/main/java/sns/member/MemberDao.java
@@ -1,7 +1,6 @@
 package sns.member;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -10,46 +9,17 @@ import java.util.ArrayList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import sns.util.DBManager;
+
 public class MemberDao {
 	
-	Connection conn = null;
-	PreparedStatement pstmt = null;
+	Connection conn;
+	PreparedStatement pstmt;
 	Logger logger = LoggerFactory.getLogger(MemberDao.class);
-
-	String jdbc_driver = "com.mysql.jdbc.Driver";
-	String jdbc_url = "jdbc:mysql://127.0.0.1:3306/hanbit1";
-	
-	// DB연결 메서드
-		void connect() {
-			try {
-				Class.forName(jdbc_driver);
-				conn = DriverManager.getConnection(jdbc_url, "developer", "developer");
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-
-		void disconnect() {
-			if (pstmt != null) {
-				try {
-					pstmt.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			} // end if(pstmt != null)
-
-			if (conn != null) {
-				try {
-					conn.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			} // end if(conn != null)
-		}
 	
 	public ArrayList<String> getNewMembers(){
+		conn = DBManager.getConnection();
 		ArrayList<String> nmembers = new ArrayList<String>();
-		connect();
 		//회원목록은 7개 까지만 가져옴
 		String sql = "select * from member order by date desc limit 0,7";
 		
@@ -62,13 +32,8 @@ public class MemberDao {
 			e.printStackTrace();
 			logger.info("Error Code : {}", e.getErrorCode());
 		} finally{
-			try{
-				pstmt.close();
-				conn.close();
-			} catch(SQLException e){
-				e.printStackTrace();
-			}//end try
-		}//end try
+			DBManager.closeConnection(pstmt, conn);
+		}
 		
 		return nmembers;
 	}//end getNewMembers()
