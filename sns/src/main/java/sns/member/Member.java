965a232cf668b6c7252e0428b3935967b201a63f
leechangsin <ycs318@naver.com>
2016-08-30 17:28:32 +0900

    #3 MemberDao의 누락된 기능 추가   1. Member     - DO인 Member과 DB테이블의 도메인명이 달라 맞춰줌   2. MemberDao     - 누락된기능인 addMember, login 기능 추가

--- sns/src/main/java/sns/member/Member.java
+++ sns/src/main/java/sns/member/Member.java
@@ -4,7 +4,7 @@ import java.sql.Date;
 
 public class Member {
 	private String name;
-	private String id;
+	private String Uid;
 	private String passwd;
 	private String email;
 	private Date date;
@@ -17,12 +17,12 @@ public class Member {
 		this.name = name;
 	}
 
-	public String getId() {
-		return id;
+	public String getUid() {
+		return Uid;
 	}
 
-	public void setId(String id) {
-		this.id = id;
+	public void setUid(String id) {
+		this.Uid = id;
 	}
 
 	public String getPasswd() {
--- sns/src/main/java/sns/member/MemberDao.java
+++ sns/src/main/java/sns/member/MemberDao.java
@@ -10,66 +10,73 @@ import java.util.ArrayList;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import text.DBManager;
+
 public class MemberDao {
-	
-	Connection conn = null;
-	PreparedStatement pstmt = null;
+	Connection conn;
+	PreparedStatement pstmt;
 	Logger logger = LoggerFactory.getLogger(MemberDao.class);
-
-	String jdbc_driver = "com.mysql.jdbc.Driver";
-	String jdbc_url = "jdbc:mysql://127.0.0.1:3306/hanbit1";
 	
-	// DB연결 메서드
-		void connect() {
-			try {
-				Class.forName(jdbc_driver);
-				conn = DriverManager.getConnection(jdbc_url, "developer", "developer");
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-
-		void disconnect() {
-			if (pstmt != null) {
-				try {
-					pstmt.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			} // end if(pstmt != null)
-
-			if (conn != null) {
-				try {
-					conn.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			} // end if(conn != null)
-		}
-	
-	public ArrayList<String> getNewMembers(){
-		ArrayList<String> nmembers = new ArrayList<String>();
-		connect();
-		//회원목록은 7개 까지만 가져옴
-		String sql = "select * from member order by date desc limit 0,7";
-		
-		try{
+	public boolean addMember(Member member) {
+		conn = DBManager.getConnection();
+		String sql = "insert into member(name, uid, passwd, email, date) values(?,?,?,?,now())";
+		try {
 			pstmt = conn.prepareStatement(sql);
-			ResultSet rs = pstmt.executeQuery();
-			while(rs.next())
-				nmembers.add(rs.getString("id"));
-		} catch(SQLException e){
+			pstmt.setString(1, member.getName());
+			pstmt.setString(2, member.getUid());
+			pstmt.setString(3, member.getPasswd());
+			pstmt.setString(4, member.getEmail());
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
 			e.printStackTrace();
 			logger.info("Error Code : {}", e.getErrorCode());
-		} finally{
-			try{
-				pstmt.close();
-				conn.close();
-			} catch(SQLException e){
-				e.printStackTrace();
-			}//end try
-		}//end try
-		
+			return false;
+		} finally {
+			DBManager.closeConnection(pstmt, conn);
+		}
+		return true;
+	}// end addMember(Member member)
+	
+	public boolean login(String uid, String passwd) {
+		conn = DBManager.getConnection();
+		String sql = "select uid, passwd from member where uid = ?";
+		ResultSet rs;
+
+		try {
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, uid);
+			rs = pstmt.executeQuery();
+			rs.next();
+			if (!rs.getString("passwd").equals(passwd))
+				return false;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			logger.info("Error Code : {}", e.getErrorCode());
+			return false;
+		} finally {
+			DBManager.closeConnection(pstmt, conn);
+		}
+		return true;
+	}
+	
+	public ArrayList<String> getNewMembers() {
+		conn = DBManager.getConnection();
+		ArrayList<String> nmembers = new ArrayList<String>();
+		// 회원목록은 7개 까지만 가져옴
+		String sql = "select * from member order by date desc limit 0,7";
+
+		try {
+			pstmt = conn.prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery();
+			while (rs.next())
+				nmembers.add(rs.getString("id"));
+		} catch (SQLException e) {
+			e.printStackTrace();
+			logger.info("Error Code : {}", e.getErrorCode());
+		} finally {
+			DBManager.closeConnection(pstmt, conn);
+		} // end try
+
 		return nmembers;
-	}//end getNewMembers()
+	}// end getNewMembers()
 }
